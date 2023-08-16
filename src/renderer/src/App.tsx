7c90cbe2a01396a2791f3b7248038b720bf64d67
Lucas Tiago <eulukasthyago@outlook.com>
2023-08-16 15:14:03 -0300

    Primeira versão

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,12 @@
 import { useEffect, useRef, useState, useCallback } from 'react'
-import { FluentProvider, Button, makeStyles, shorthands } from '@fluentui/react-components'
+import {
+  FluentProvider,
+  Button,
+  makeStyles,
+  shorthands,
+  Select,
+  useId
+} from '@fluentui/react-components'
 import { darkTheme } from './settings/theme'
 
 const useStyle = makeStyles({
@@ -7,62 +14,147 @@ const useStyle = makeStyles({
     display: 'flex',
     columnGap: '15px',
     ...shorthands.padding('20px', '20px', '20px', '20px')
+  },
+
+  princialContainer: {
+    ...shorthands.flex(1),
+    display: 'flex',
+    flexDirection: 'column'
   }
 })
 
 function App(): JSX.Element {
   const styles = useStyle()
-  let webc;
   const webcamRef = useRef<HTMLVideoElement>(null)
-  const webcamStream = useRef<MediaStream | null>(null)
 
-  // const [webcam, setWebcam] = useState<MediaStream | null>()
+  const [isRecording, setIsRording] = useState(false)
+  const [isWebcamOpened, setIsWebcamOpened] = useState(false)
 
-  const handleStopWebcam = useCallback(() => {
-    webcamStream.current?.getTracks().forEach((tracks) => {
-      console.log(tracks)
-      tracks.stop()
-    })
-    console.log(webcamStream.current?.getTracks())
-  }, [webcamStream])
+  const [videoStream, setVideoStream] = useState<MediaStream>()
+  const [micList, setMicList] = useState<MediaDeviceInfo[]>()
+  const [micSelected, setMicSelected] = useState<ConstrainDOMString>()
 
-  const handleStartWebcam = () => {
-    webc?.getTracks().forEach((tracks) => {
-      tracks
-    })
-    console.log(webc)
-  }
-
-  useEffect(() => {
+  const handleStartWebcam = useCallback(() => {
     if (navigator && navigator.mediaDevices && 'getUserMedia' in navigator.mediaDevices) {
       console.log('Suportado')
       navigator.mediaDevices
-        .getUserMedia({ video: { width: 1080 }, audio: false })
+        .getUserMedia({
+          video: { width: 1080 },
+          audio: {
+            deviceId: micSelected,
+            autoGainControl: false
+          }
+        })
         .then((stream) => {
           if (webcamRef.current) webcamRef.current.srcObject = stream
-          if(webcamStream.current) webcamStream.current = stream
-          // setWebcam(stream)
-          webc = stream
+          setVideoStream(stream)
+          setIsWebcamOpened(true)
+          console.log('Mic Start', micSelected)
         })
     } else {
       console.log('Não Suportado')
     }
+  }, [videoStream, isWebcamOpened, micSelected])
+
+  const handleStopWebcam = useCallback(() => {
+    if (videoStream?.getVideoTracks() && videoStream.getVideoTracks()[0]) {
+      videoStream.getVideoTracks()[0].stop()
+      setIsWebcamOpened(!isWebcamOpened)
+    }
+    if (videoStream?.getAudioTracks()[0]) {
+      videoStream?.getAudioTracks()[0].stop()
+    }
+  }, [videoStream, isWebcamOpened])
+
+  const handleVideoStartRecord = useCallback(() => {
+    setIsRording(!isRecording)
+    console.log(isWebcamOpened)
+  }, [isRecording, isWebcamOpened])
+
+  const handleVideoStopRecording = useCallback(() => {
+    setIsRording(!isRecording)
+    console.log(isWebcamOpened)
+  }, [isRecording, isWebcamOpened])
+
+  const handleChangeMic = useCallback(
+    (event: React.ChangeEvent<HTMLSelectElement>) => {
+      setMicSelected(() => event.target.value)
+    },
+    [micSelected]
+  )
+
+  useEffect(() => {
+    if (navigator && navigator.mediaDevices) {
+      navigator.mediaDevices.enumerateDevices().then((devices) => {
+        const audioDevices = devices.filter((device) => device.kind === 'audioinput')
+        if (!micSelected) {
+          setMicSelected(() => audioDevices[0].deviceId)
+        }
+        setMicList(audioDevices)
+      })
+    }
   }, [])
 
+  const selectMicId = useId()
+
   return (
-    <FluentProvider theme={darkTheme} style={{ minHeight: '100%' }}>
-      <video
-        id="videoWebcam"
-        style={{ width: '100%', maxWidth: 1024 }}
-        ref={webcamRef}
-        autoPlay
-      ></video>
+    <FluentProvider theme={darkTheme} className={styles.princialContainer}>
+      <div style={{ flex: 1 }}>
+        <video
+          id="videoWebcam"
+          style={{ width: '100%', maxWidth: 1024, aspectRatio: 16 / 9, background: 'black' }}
+          ref={webcamRef}
+          autoPlay
+        ></video>
+      </div>
       <div className={styles.innerWapper}>
-        <Button appearance="primary">Record</Button>
-        <Button appearance="secondary">Stop</Button>
-        <Button appearance="secondary" onClick={handleStopWebcam}>
-          Stop Webcam
-        </Button>
+        {isWebcamOpened ? (
+          !isRecording ? (
+            <Button
+              style={{ background: 'green' }}
+              appearance="primary"
+              onClick={handleVideoStartRecord}
+            >
+              Record
+            </Button>
+          ) : (
+            <Button
+              style={{ background: 'red' }}
+              appearance="primary"
+              onClick={handleVideoStopRecording}
+            >
+              Stop
+            </Button>
+          )
+        ) : (
+          <Button appearance="primary" disabled>
+            Record
+          </Button>
+        )}
+        {!isRecording ? (
+          !isWebcamOpened ? (
+            <Button appearance="secondary" onClick={handleStartWebcam}>
+              Start Webcam
+            </Button>
+          ) : (
+            <Button appearance="secondary" onClick={handleStopWebcam}>
+              Stop Webcam
+            </Button>
+          )
+        ) : (
+          <Button appearance="secondary" disabled>
+            Stop Webcam
+          </Button>
+        )}
+        <Select id={selectMicId} onChange={handleChangeMic}>
+          {micList?.map((item) => {
+            return (
+              <option key={item.deviceId} value={item.deviceId}>
+                {item.label}
+              </option>
+            )
+          })}
+        </Select>
       </div>
     </FluentProvider>
   )
